e9d55241c67f8c15768ebc2a7980607e8d59c93d
2301887641 <15863549041#126.com>
2017-03-17 09:45:58 +0800

    提交链接添加模块

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -12,13 +12,14 @@ namespace app\index\controller;
 use think\Controller;
 use think\Hook;
 use think\Request;
+use think\Session;
 
 /**
  * 基本操作 公共类库 比如 判断是不是已经登陆了
  */
 class Base extends Controller
 {
-    protected $Rrequest;
+    protected $request;
     const error = 'failed';
     const success = 'success';
 
@@ -31,7 +32,7 @@ class Base extends Controller
     public function _initialize()
     {
         parent::_initialize();
-        $this->request=Request::instance();
+        $this->request = Request::instance();
         //需要判断是不是已经登陆了
         $action = array();
         //调用钩子方法 判断是不是已经登录
@@ -39,6 +40,7 @@ class Base extends Controller
             //表示没有登陆的操作
             $this->redirect('login/index', ['alert' => '请先登陆系统。']);
         }
+        $this->checkToken();
     }
 
     /**
@@ -49,6 +51,64 @@ class Base extends Controller
     {
         $this->assign('modal_id', Request::instance()->param('modal_id'));
         $this->assign('datagrid_id', Request::instance()->param('datagrid_id'));
+        $this->assign("token", $this->buildToken());
+    }
+
+    /**
+     * 生成token
+     * @return string
+     */
+    public function buildToken()
+    {
+        $md5 = md5(md5(time() . chr(rand(65, 90)) . rand(1, 1000)) . rand(1, 1000));
+        Session::set("token", $md5);
+        return $md5;
+    }
+
+    /**
+     * 验证token
+     * @return array
+     */
+    public function checkToken()
+    {
+        $token = Session::get("token");
+        $getToken = $this->request->post("token");
+        if (empty($getToken)) {
+            return true;
+        }
+        if ($token != $getToken) {
+            exit(json_encode([
+                "status" => self::error,
+                "msg" => "请不要重复提交",
+                "title" => "数据提交"
+            ]));
+        }
+    }
+
+    /**
+     * 删除token
+     */
+    public function deleteToken()
+    {
+        Session::delete("token");
+    }
+
+    /**
+     * 统一的信息打印
+     * @param $msg
+     * @param $title
+     * @param string $status
+     */
+    public function msg($msg,$title,$status="success")
+    {
+        if($status=="success"){
+            $this->deleteToken();
+        }
+        exit(json_encode([
+            "status" => $status,
+            "msg" => $msg,
+            "title" => $title
+        ]));
     }
 
 }
--- application/index/controller/Link.php
+++ application/index/controller/Link.php
@@ -1,11 +1,15 @@
 <?php
 /**
+ * 链接控制器
  * Created by PhpStorm.
- * User: qiangbi
+ * User: suiguozhen  15863549041@126.com
  * Date: 17-3-16
  * Time: 下午3:21
  */
 namespace app\index\controller;
+
+use think\Db;
+use think\Validate;
 
 class Link extends Base
 {
@@ -18,9 +22,39 @@ class Link extends Base
         return view();
     }
 
+    /**
+     *添加链接页面
+     * @return \think\response\View
+     */
     public function add()
     {
         $this->get_assign();
         return view();
+    }
+
+    /**
+     * 链接添加 数据
+     * @return array
+     */
+    public function addData()
+    {
+        $rule = [
+            ["link_title", "require", "请输入名称"],
+            ["link_title","unique:link","名称不能重复"],
+            ["redirect_url", "require", "请输入链接"]
+        ];
+        $validate = new Validate($rule);
+        $data = $this->request->post();
+        if (!$validate->check($data)) {
+            $this->msg($validate->getError(), "添加链接", self::error);
+        }
+        if (!Db::name("link")->insert($data)) {
+            $this->msg("添加失败", "添加链接", self::error);
+        }
+        $this->msg("添加成功", "添加链接");
+    }
+    public function getData()
+    {
+        
     }
 }
