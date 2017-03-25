16fa81f59e1b382dd06f7289b71498b7c5d860c1
2301887641 <2301887641@126.com>
2017-03-25 11:04:22 +0800

    test

--- application/index/controller/Unsubscribeemail.php
+++ application/index/controller/Unsubscribeemail.php
@@ -46,7 +46,6 @@ class Unsubscribeemail extends Controller
         if (empty($sendRecord->ip_serialize)) {
             $sendRecord->read_num++;
             $ip_info = $ip_arr["area"] . "-" . $ip_arr["region"] . "-" . $ip_arr["city"] . "-" . $ip_arr["county"];
-            $ip = $ip_arr["ip"];
             $ipSerialize = [
                 0 => [
                     "ip_info" => $ip_info,
@@ -58,10 +57,9 @@ class Unsubscribeemail extends Controller
         } else {
             $ipSerialize = unserialize($sendRecord->ip_serialize);
             $ip_info = $ip_arr["area"] . "-" . $ip_arr["region"] . "-" . $ip_arr["city"] . "-" . $ip_arr["county"];
-            $ip = $ip_arr["ip"];
             $ipSerialize[] = [
                 "ip_info" => $ip_info,
-                "ip" => $ip
+                "ip" => $_SERVER["REMOTE_ADDR"]
             ];
             $sendRecord->ip_serialize = serialize($ipSerialize);
             $sendRecord->read_num++;
