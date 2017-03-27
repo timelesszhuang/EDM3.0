55613d522ecbfd7fcd10b3317ad6b3f7375c1158
2301887641 <2301887641@126.com>
2017-03-27 10:55:02 +0800

    修改打印

--- application/common/controller/EmailUtil.php
+++ application/common/controller/EmailUtil.php
@@ -88,6 +88,7 @@ class EmailUtil
         curl_setopt($curl, CURLOPT_TIMEOUT, 2);
         //这个就是超时时间了
         $data = curl_exec($curl);
+        file_put_contents("ip.txt",$data."\r\n",FILE_APPEND);
         return json_decode($data, true);
     }
 
